485a33d30c4de24b2b0b42ff6144daed63fe0b5a
arfaghif <arfasen17@gmail.com>
2019-09-30 22:24:37 +0700

    nambahin komen

--- theSims.h
+++ theSims.h
@@ -15,87 +15,127 @@ typedef struct{
 
 /*** Konstruktor ***/
 void start(SIMS *charSims);
-//Prosedur untuk memulai permainan, yakni mengeset status karakter menjadi 0 hygiene | 10 energy | 0 fun
+//I.S *charSIMS terdefinisi
+//F.S status karakter menjadi 0 hygiene | 10 energy | 0 fun
+//Prosedur untuk memulai permainan
 
 /*** Selektor ***/
 int getHygiene(SIMS charSims);
-//Fungsi untuk mendapatkan nilai status hygiene karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mendapatkan nilai status hygiene karakter
 
 int getEnergy(SIMS charSims);
-//Fungsi untuk mendapatkan nilai status energy karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mendapatkan nilai status energy karakter
 
 int getFun(SIMS charSims);
-//Fungsi untuk mendapatkan nilai status fun karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mendapatkan nilai status fun karakter
 
 void setHygiene(SIMS *charSims, int hy);
-//Prosedur untuk mengubah nilai status hygiene karakter
+//Prosedur untuk mengubah nilai status hy karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan hy merupakan masukan status valid
+//F.S Nilai hygiene player menjadi hy
 
 void setEnergy(SIMS *charSims, int en);
-//Prosedur untuk mengubah nilai status energy karakter
+//Prosedur untuk mengubah nilai status en karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan en merupakan masukan status valid
+//F.S Nilai energy player menjadi en
 
 void setFun(SIMS *charSims, int fun);
 //Prosedur untuk mengubah nilai status fun karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan fun merupakan masukan status valid
+//F.S Nilai fun player menjadi fun
 
 void setStatus(SIMS *charSims, int hy, int en, int fun);
 //Prosedur untuk mengubah seluruh nilai status karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan en,hy,fun merupakan masukan status valid
+//F.S Nilai hygiene player menjadi hy, energy menjadi en, fun menjadi fun
 
 void addHygiene(SIMS *charSims, int hy);
 //Prosedur untuk menambahkan nilai status hygiene karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan ketika dilakukan pertambahan status tetap valid
+//F.S Nilai hygiene palyer bertambah hy
 
 void addEnergy(SIMS *charSims, int en);
 //Prosedur untuk menambahkan nilai status energy karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan ketika dilakukan pertambahan status tetap valid
+//F.S Nilai energy palyer bertambah en
 
 void addFun(SIMS *charSims, int fun);
 //Prosedur untuk menambahkan nilai status fun karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan ketika dilakukan pertambahan status tetap valid
+//F.S Nilai fun palyer bertambah fun
 
 void addStatus(SIMS *charSims, int hy, int en, int fun);
 //Prosedur untuk menambahkan seluruh nilai status karakter
+//I.S charSIMS terdefinisi dan setiap statusnya valid dan ketika dilakukan pertambahan status tetap valid
+//F.S Nilai hygiene palyer bertambah hy, energy bertambah en, fun bertambah fun
 
 void outCurStatus(SIMS charSIMS);
-//Prosedur untuk mencetak ke layar status karakter sekarang
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak ke layar status karakter sekarang
 
 boolean isAttributeValid(int hy,int en, int fun);
-//Menghasilkan true jika semua nilai atribut valid
+//I.S charSIMS terdefinisi
+//F.S Menghasilkan true jika semua nilai atribut valid
 
 boolean isAddAttributeValid(SIMS charSIMS, int hy, int en, int fun);
-//Menghasilkan true jika semua nilai atribut tetap valid jika dilakukan penambahan
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S Menghasilkan true jika semua nilai atribut tetap valid jika dilakukan penambahan
 
 void tidur(SIMS *charSims, int jenis);
 //Aksi tidur mempunyai 2 pilihan:
 //siang(+10 energy) || malam(+15 energy)
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status sesuai pilihan
 
 void makan(SIMS *charSims, int jenis);
 //Aksi makan mempunyai 4 pilihan:
 //hamburger(+5 energy) || pizza(+10 energy) || steak(+15 energy) || beans(+15 energy)
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status sesuai pilihan
 
 void minum(SIMS *charSims, int jenis);
 //Aksi minum mempunyai 3 pilihan:
 //air(-5 hygiene) || kopi(+5 energy, -10 hygiene) || jus(+10 energy, -5 hygiene)
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status sesuai pilihan
 
 void buangAir(SIMS *charSims, int jenis);
 //Aksi buangAir mempunyai 2 pilihan:
 //kecil(+5 hygiene) || besar(+10 hygiene, -5 energy)
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status sesuai pilihan
 
 void sosialKafe(SIMS *charSims);
-//Aksi sosialKafe menyebabkan +15 fun, -10 energy, -5 hygiene
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status fun  +15 fun, energy -10 dan hygiene -5
 
 void sosialMedia(SIMS *charSims);
-//Aksi sosialMedia menyebabkan +10 fun, -10 energy
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status fun 10 fun dan energy -10
 
 void mainKomputer(SIMS *charSims);
-//Aksi mainKomputer menyebabkan +15 fun
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status fun 15
 
 void mandi(SIMS *charSims);
-//Aksi mandi menyebabkan +15 hygiene, -5 energy
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status hygiene 15 hygiene dan energy -5
 
 void cuciTangan(SIMS *charSims);
-//Aksi cuciTangan menyebabkan +5 hygiene
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status hygiene +5
 
 void musikRadio(SIMS *charSims);
-//Aksi musikRadio menyebabkan +10 fun, -5 energy
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status fun 10 dan energy  -5
 
 void membaca(SIMS *charSims, int jenis);
 //Aksi membaca memiliki 2 pilihan:
 //koran(+5 fun, -5 energy) || novel (+10 fun, -5 energy)
+//I.S charSIMS terdefinisi dan setiap statusnya valid
+//F.S mencetak "Aksi tidak valid" atau menambahkan nilai status sesuai pilihan
 
 #endif
