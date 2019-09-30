ef7479187ebbbfca4bda506c917d82d627041775
Daffa Pratama <daffapratama8c29@gmail.com>
2019-09-30 20:24:48 +0700

    komentar

--- theSims.h
+++ theSims.h
@@ -1,4 +1,4 @@
-// DEFINISI TYPE SIMS
+// DEFINISI TYPE THESIMS
 #ifndef THESIMS_H
 #define THESIMS_H
 
@@ -13,33 +13,46 @@ typedef struct{
 #define en(charSims)   (charSims).energy
 #define fun(charSims)   (charSims).fun
 
-/*** Konstkurtor ***/
+/*** Konstruktor ***/
 void start(SIMS *charSims);
+//Prosedur untuk memulai permainan, yakni mengeset status karakter menjadi 0 hygiene | 10 energy | 0 fun
 
 /*** Selektor ***/
 int getHygiene(SIMS charSims);
+//Fungsi untuk mendapatkan nilai status hygiene karakter
 
 int getEnergy(SIMS charSims);
+//Fungsi untuk mendapatkan nilai status energy karakter
 
 int getFun(SIMS charSims);
+//Fungsi untuk mendapatkan nilai status fun karakter
 
 void setHygiene(SIMS *charSims, int hy);
+//Prosedur untuk mengubah nilai status hygiene karakter
 
 void setEnergy(SIMS *charSims, int en);
+//Prosedur untuk mengubah nilai status energy karakter
 
 void setFun(SIMS *charSims, int fun);
+//Prosedur untuk mengubah nilai status fun karakter
 
 void setStatus(SIMS *charSims, int hy, int en, int fun);
+//Prosedur untuk mengubah seluruh nilai status karakter
 
 void addHygiene(SIMS *charSims, int hy);
+//Prosedur untuk menambahkan nilai status hygiene karakter
 
 void addEnergy(SIMS *charSims, int en);
+//Prosedur untuk menambahkan nilai status energy karakter
 
 void addFun(SIMS *charSims, int fun);
+//Prosedur untuk menambahkan nilai status fun karakter
 
 void addStatus(SIMS *charSims, int hy, int en, int fun);
+//Prosedur untuk menambahkan seluruh nilai status karakter
 
 void outCurStatus(SIMS charSIMS);
+//Prosedur untuk mencetak ke layar status karakter sekarang
 
 void tidur(SIMS *charSims, int jenis);
 //Aksi tidur mempunyai 2 pilihan:
@@ -76,5 +89,7 @@ void musikRadio(SIMS *charSims);
 //Aksi musikRadio menyebabkan +10 fun, -5 energy
 
 void membaca(SIMS *charSims, int jenis);
+//Aksi membaca memiliki 2 pilihan:
+//koran(+5 fun, -5 energy) || novel (+10 fun, -5 energy)
 
 #endif
